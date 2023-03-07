42dfaff57ceefe1ddbb4618b34d8797cb2253fbb
Michelly <michelly.daiana@hotmail.com>
2023-03-07 14:56:51 -0300

    formatting

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,4 +1,4 @@
 SELECT
-(select  COUNT(*) from musics) AS cancoes,
-(select  COUNT(*) from artists) AS artistas,
-(select  COUNT(*) from albums) AS albuns;
+  (select  COUNT(*) from musics) AS cancoes,
+  (select  COUNT(*) from artists) AS artistas,
+  (select  COUNT(*) from albums) AS albuns;
---
+++ challenges/desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+	  ROUND(MIN(plans.value_plan), 2) AS faturamento_minimo,
+    ROUND(MAX(plans.value_plan), 2) AS faturamento_maximo,
+    ROUND(AVG(plans.value_plan), 2) AS faturamento_medio,
+    ROUND(SUM(plans.value_plan), 2) AS faturamento_total
+FROM plans
+INNER JOIN users
+ON plan_id = plans.id;
