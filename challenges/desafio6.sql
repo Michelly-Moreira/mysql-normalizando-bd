b539a5f76a59963ace936a77503fc4c5821c5c70
Michelly <michelly.daiana@hotmail.com>
2023-03-08 01:54:30 -0300

    atualizando

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,5 +1,5 @@
 SELECT
-	  ROUND(MIN(plans.value_plan), 2) AS faturamento_minimo,
+	ROUND(MIN(plans.value_plan), 2) AS faturamento_minimo,
     ROUND(MAX(plans.value_plan), 2) AS faturamento_maximo,
     ROUND(AVG(plans.value_plan), 2) AS faturamento_medio,
     ROUND(SUM(plans.value_plan), 2) AS faturamento_total
