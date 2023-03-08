fb03145f9f4fc5f143be3815aa630586b2c4288b
Michelly <michelly.daiana@hotmail.com>
2023-03-08 13:56:59 -0300

    task 3 approved

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,15 +1,11 @@
--- tempo de cada música
-SELECT ROUND(musics.duration_in_seconds / 60, 2) AS total_minutos
-FROM musics;
-
 SELECT
 	users.user_name AS pessoa_usuaria,
 	COUNT(reproduction_historic.user_people_historic) AS musicas_ouvidas,
-	ROUND(SUM(musics.duration_in_seconds / 60), 2) AS total_minutos
+	FORMAT(ROUND(SUM(musics.duration_in_seconds / 60), 2), 2) AS total_minutos
 FROM users
 INNER JOIN reproduction_historic
 ON users.id = reproduction_historic.user_people_historic
 INNER JOIN musics
-ON reproduction_historic.user_people_historic = musics.id
+ON reproduction_historic.music_played = musics.id
 GROUP BY pessoa_usuaria
 ORDER BY pessoa_usuaria ASC;
